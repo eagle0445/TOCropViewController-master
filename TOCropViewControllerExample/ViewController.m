558df15e633ee778b0acbf0def922b85d096c968
帅帅 <26230197@qq.com>
2016-08-16 11:35:33 +0800

    限制图片大小

--- TOCropViewControllerExample/ViewController.m
+++ TOCropViewControllerExample/ViewController.m
@@ -25,6 +25,23 @@
 
 @implementation ViewController
 
+
+#pragma mark -- 压缩图片至100k以下
+-(NSData *)imageData:(UIImage *)myimage
+{
+    NSData *data=UIImageJPEGRepresentation(myimage, 1.0);
+    if (data.length>100*1024) {
+        if (data.length>1024*1024) {//1M以及以上
+            data=UIImageJPEGRepresentation(myimage, 0.01);
+        }else if (data.length>512*1024) {//0.5M-1M
+            data=UIImageJPEGRepresentation(myimage, 0.09);
+        }else if (data.length>200*1024) {//0.25M-0.5M
+            data=UIImageJPEGRepresentation(myimage, 0.16);
+        }
+    }
+    return data;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"TOCropViewController";
